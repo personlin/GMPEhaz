c2d19d78d48129e6b04a62e59a09ff179548cc6c
Person <personlin@gmail.com>
2016-10-04 08:53:01 +0800

    add LL08 and Lin11 function

--- R/LL08.R
+++ R/LL08.R
@@ -1,5 +1,44 @@
 #' GMPE function for Lin and Lee(2008)
 #'
+#' \code{LL08} returns the ground-motion prediction (Rock or Soil site base on Vs30)
+#' with it sigma of Lin and Lee(2008) GMPE.
+#'
+#'Lin, P., and C. Lee (2008), Ground-motion attenuation relationships for subduction-zone earthquakes in
+#'northeastern taiwan, Bulletin of the Seismological Society of America, 98(1), 220-240.
+#'\url{http://dx.doi.org/10.1785/0120060002}
+#'
+#' @param Mag Earthquake momnet magnitude, Numeric.
+#' @param Rrup Rupture distance(km), Numeric.
+#' @param depth hypocentral depth(km).
+#' @param ftype fytpe=0 for interface, ftype=1 for intraslab
+#' @param Prd Period of spectral acceleration.
+#' @param Vs30 Vs30(m/s)
+#'
+#' @return A list will be return, including mag, Rrup, specT, period, lnY, sigma, iflag, depth, ftype.
+#'
+#' @examples
+#' LL08(6, 20, 10, 0, 0, 760)
+#' LL08(7, 10, 10, 0, 0, 760)
+#'
+#' @export
+LL08 <- function(Mag, Rrup, depth, ftype, Prd, Vs30=760) {
+  if (Vs30 > 360){
+    retvals <- .Fortran("LinLee08rock", mag=as.single(Mag), rupdist=as.single(Rrup), specT=as.single(Prd),
+                        period=as.single(0), lnY=as.single(0.1), sigma=as.single(0.1), iflag=as.integer(1),
+                        depth=as.single(depth), ftype=as.single(ftype)
+    )
+  } else {
+    retvals <- .Fortran("LinLee08soil", mag=as.single(Mag), rupdist=as.single(Rrup), specT=as.single(Prd),
+                        period=as.single(0), lnY=as.single(0.1), sigma=as.single(0.1), iflag=as.integer(1),
+                        depth=as.single(depth), ftype=as.single(ftype)
+    )
+  }
+  names(retvals) <- c("mag", "Rrup", "specT", "period", "lnY", "sigma", "iflag", "depth", "ftype")
+  return(retvals)
+}
+
+#' GMPE function for Lin and Lee(2008)
+#'
 #' \code{LL08Rock} returns the ground-motion prediction (rock site) with it sigma of Lin and Lee(2008) GMPE.
 #'
 #'Lin, P., and C. Lee (2008), Ground-motion attenuation relationships for subduction-zone earthquakes in
@@ -58,3 +97,5 @@ LL08Soil <- function(Mag, Rrup, depth, ftype, Prd) {
   names(retvals) <- c("mag", "Rrup", "specT", "period", "lnY", "sigma", "iflag", "depth", "ftype")
   return(retvals)
 }
+
+
--- R/Lin11.R
+++ R/Lin11.R
@@ -1,5 +1,49 @@
 #' GMPE function for Lin et al.(2011)
 #'
+#' \code{Lin11} returns the ground-motion prediction (rock or soil site base on Vs30)
+#' with it sigma of Lin et al.(2011) GMPE.
+#'
+#'Lin, P.-S., C.-T. Lee, C.-T. Cheng, and C.-H. Sung (2011), Response spectral attenuation relations
+#'for shallow crustal earthquakes in taiwan, Engineering Geology, 121(3-4), 150-164.
+#'\url{http://dx.doi.org/10.1016/j.enggeo.2011.04.019}
+#'
+#' @param Mag Earthquake momnet magnitude, Numeric.
+#' @param Rrup Rupture distance(km), Numeric.
+#' @param Prd Period of spectral acceleration.
+#' @param hwflag hanging-wall flag, 1 for hanging-wall.
+#' @param Vs30 Vs30(m/s)
+#'
+#' @return A list will be return, including mag, Rrup, specT, period, lnY, sigma, iflag.
+#'
+#' @examples
+#' Lin11(6, 20, 0, 0, 760)
+#' Lin11(7, 10, 0, 0, 760)
+#'
+#' @export
+Lin11 <- function(Mag, Rrup, Prd, hwflag=0, Vs30=760){
+  if(Vs30 >360){
+    if(hwflag==0){
+      retvals <- .Fortran("Lin_fw_rock", mag=as.single(Mag), rupdist=as.single(Rrup), specT=as.single(Prd),
+                          period=as.single(0), lnY=as.single(0.1), sigma=as.single(0.1), iflag=as.integer(1))
+    }else{
+      retvals <- .Fortran("Lin_hw_rock", mag=as.single(Mag), rupdist=as.single(Rrup), specT=as.single(Prd),
+                          period=as.single(0), lnY=as.single(0.1), sigma=as.single(1), iflag=as.integer(1))
+    }
+  }else{
+    if(hwflag==0){
+      retvals <- .Fortran("Lin_fw_soil", mag=as.single(Mag), rupdist=as.single(Rrup), specT=as.single(Prd),
+                          period=as.single(0), lnY=as.single(0.1), sigma=as.single(0.1), iflag=as.integer(1))
+    }else{
+      retvals <- .Fortran("Lin_hw_soil", mag=as.single(Mag), rupdist=as.single(Rrup), specT=as.single(Prd),
+                          period=as.single(0), lnY=as.single(0.1), sigma=as.single(1), iflag=as.integer(1))
+    }
+  }
+  names(retvals) <- c("mag", "Rrup", "specT", "period", "lnY", "sigma", "iflag")
+  return(retvals)
+}
+
+#' GMPE function for Lin et al.(2011)
+#'
 #' \code{Lin11Rock} returns the ground-motion prediction (rock site) with it sigma of Lin et al.(2011) GMPE.
 #'
 #'Lin, P.-S., C.-T. Lee, C.-T. Cheng, and C.-H. Sung (2011), Response spectral attenuation relations
