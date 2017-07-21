86cf16de27b7fb66890fcd04f3401ce7b76999d5
Person Lin <personlin@gmail.com>
2017-07-21 17:18:47 +0800

    add cnames

--- R/data.R
+++ R/data.R
@@ -3,3 +3,291 @@
 #' @format A vector with 105 values.
 #'
 "periods.T"
+
+#' Variable name of AB03 GMPEs.
+#'
+#' @format Vectors of AB03 GMPE.
+#'
+"cnames.AB03"
+
+#' Variable name of AC10 GMPEs.
+#'
+#' @format Vectors of AC10 GMPE.
+#'
+"cnames.AC10"
+
+#' Variable name of AGA16.tw.C01 GMPEs.
+#'
+#' @format Vectors of AGA16.tw.C01 GMPE.
+#'
+"cnames.AGA16.tw.C01"
+
+#' Variable name of AM09 GMPEs.
+#'
+#' @format Vectors of AM09 GMPE.
+#'
+"cnames.AM09"
+
+#' Variable name of Arroyo2010 GMPEs.
+#'
+#' @format Vectors of Arroyo2010 GMPE.
+#'
+"cnames.Arroyo2010"
+
+#' Variable name of ASB14 GMPEs.
+#'
+#' @format Vectors of ASB14 GMPE.
+#'
+"cnames.ASB14"
+
+#' Variable name of ASB14.tw.B01 GMPEs.
+#'
+#' @format Vectors of ASB14.tw.B01 GMPE.
+#'
+"cnames.ASB14.tw.B01"
+
+#' Variable name of ASB14.tw.C01 GMPEs.
+#'
+#' @format Vectors of ASB14.tw.C01 GMPE.
+#'
+"cnames.ASB14.tw.C01"
+
+#' Variable name of ASK14 GMPEs.
+#'
+#' @format Vectors of ASK14 GMPE.
+#'
+"cnames.ASK14"
+
+#' Variable name of ASK14.tw.B01 GMPEs.
+#'
+#' @format Vectors of ASK14.tw.B01 GMPE.
+#'
+"cnames.ASK14.tw.B01"
+
+#' Variable name of BCHydroSubV3 GMPEs.
+#'
+#' @format Vectors of BCHydroSubV3 GMPE.
+#'
+"cnames.BCHydroSubV3"
+
+#' Variable name of BI14 GMPEs.
+#'
+#' @format Vectors of BI14 GMPE.
+#'
+"cnames.BI14"
+
+#' Variable name of BI14.tw.B01 GMPEs.
+#'
+#' @format Vectors of BI14.tw.B01 GMPE.
+#'
+"cnames.BI14.tw.B01"
+
+#' Variable name of BI14.tw.C01 GMPEs.
+#'
+#' @format Vectors of BI14.tw.C01 GMPE.
+#'
+"cnames.BI14.tw.C01"
+
+#' Variable name of BSSA14 GMPEs.
+#'
+#' @format Vectors of BSSA14 GMPE.
+#'
+"cnames.BSSA14"
+
+#' Variable name of BSSA14.tw.B01 GMPEs.
+#'
+#' @format Vectors of BSSA14.tw.B01 GMPE.
+#'
+"cnames.BSSA14.tw.B01"
+
+#' Variable name of BSSA14.tw.C01 GMPEs.
+#'
+#' @format Vectors of BSSA14.tw.C01 GMPE.
+#'
+"cnames.BSSA14.tw.C01"
+
+#' Variable name of Camp03H GMPEs.
+#'
+#' @format Vectors of Camp03H GMPE.
+#'
+"cnames.Camp03H"
+
+#' Variable name of CB14 GMPEs.
+#'
+#' @format Vectors of CB14 GMPE.
+#'
+"cnames.CB14"
+
+#' Variable name of CB14.tw.C01 GMPEs.
+#'
+#' @format Vectors of CB14.tw.C01 GMPE.
+#'
+"cnames.CB14.tw.C01"
+
+#' Variable name of Chao2017 GMPEs.
+#'
+#' @format Vectors of Chao2017 GMPE.
+#'
+"cnames.Chao2017"
+
+#' Variable name of CY08 GMPEs.
+#'
+#' @format Vectors of CY08 GMPE.
+#'
+"cnames.CY08"
+
+#' Variable name of CY14 GMPEs.
+#'
+#' @format Vectors of CY14 GMPE.
+#'
+"cnames.CY14"
+
+#' Variable name of CY14.tw.B0 GMPEs.
+#'
+#' @format Vectors of CY14.tw.B0 GMPE.
+#'
+"cnames.CY14.tw.B01"
+
+#' Variable name of CY14.tw.C01 GMPEs.
+#'
+#' @format Vectors of CY14.tw.C01 GMPE.
+#'
+"cnames.CY14.tw.C01"
+
+#' Variable name of Garcia05 GMPEs.
+#'
+#' @format Vectors of Garcia05 GMPE.
+#'
+"cnames.Garcia05"
+
+#' Variable name of GK16 GMPEs.
+#'
+#' @format Vectors of GK16 GMPE.
+#'
+"cnames.GK16"
+
+#' Variable name of Gregor06 GMPEs.
+#'
+#' @format Vectors of Gregor06 GMPE.
+#'
+"cnames.Gregor06"
+
+#' Variable name of I14 GMPEs.
+#'
+#' @format Vectors of I14 GMPE.
+#'
+"cnames.I14"
+
+#' Variable name of I14.tw.B01 GMPEs.
+#'
+#' @format Vectors of I14.tw.B01 GMPE.
+#'
+"cnames.I14.tw.B01"
+
+#' Variable name of I14.tw.C01 GMPEs.
+#'
+#' @format Vectors of I14.tw.C01 GMPE.
+#'
+"cnames.I14.tw.C01"
+
+#' Variable name of KAAH15 GMPEs.
+#'
+#' @format Vectors of KAAH15 GMPE.
+#'
+"cnames.KAAH15"
+
+#' Variable name of Kanno06 GMPEs.
+#'
+#' @format Vectors of Kanno06 GMPE.
+#'
+"cnames.Kanno06"
+
+#' Variable name of Lin09 GMPEs.
+#'
+#' @format Vectors of Lin09 GMPE.
+#'
+"cnames.Lin09"
+
+#' Variable name of Lin11Rock GMPEs.
+#'
+#' @format Vectors of Lin11Rock GMPE.
+#'
+"cnames.Lin11Rock"
+
+#' Variable name of Lin11Soil GMPEs.
+#'
+#' @format Vectors of Lin11Soil GMPE.
+#'
+"cnames.Lin11Soil"
+
+#' Variable name of LL08 GMPEs.
+#'
+#' @format Vectors of LL08 GMPE.
+#'
+"cnames.LL08"
+
+#' Variable name of LL08Rock GMPEs.
+#'
+#' @format Vectors of LL08Rock GMPE.
+#'
+"cnames.LL08Rock"
+
+#' Variable name of LL08Soil GMPEs.
+#'
+#' @format Vectors of LL08Soil GMPE.
+#'
+"cnames.LL08Soil"
+
+#' Variable name of MBR17 GMPEs.
+#'
+#' @format Vectors of MBR17 GMPE.
+#'
+"cnames.MBR17"
+
+#' Variable name of Mcverry2006Sub GMPEs.
+#'
+#' @format Vectors of Mcverry2006Sub GMPE.
+#'
+"cnames.Mcverry2006Sub"
+
+#' Variable name of Sinotech12 GMPEs.
+#'
+#' @format Vectors of Sinotech12 GMPE.
+#'
+"cnames.Sinotech12"
+
+#' Variable name of Youngs97Rock GMPEs.
+#'
+#' @format Vectors of Youngs97Rock GMPE.
+#'
+"cnames.Youngs97Rock"
+
+#' Variable name of Zh06 GMPEs.
+#'
+#' @format Vectors of Zh06 GMPE.
+#'
+"cnames.Zh06"
+
+#' Variable name of Zh16Cru GMPEs.
+#'
+#' @format Vectors of Zh16Cru GMPE.
+#'
+"cnames.Zh16Cru"
+
+#' Variable name of Zh16Inter GMPEs.
+#'
+#' @format Vectors of Zh16Inter GMPE.
+#'
+"cnames.Zh16Inter"
+
+#' Variable name of Zh16Slab GMPEs.
+#'
+#' @format Vectors of Zh16Slab GMPE.
+#'
+"cnames.Zh16Slab"
+
+#' Variable name of Zh16Sub GMPEs.
+#'
+#' @format Vectors of Zh16Sub GMPE.
+#'
+"cnames.Zh16Sub"
